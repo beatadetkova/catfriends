625704309d53931d179962185acc460661670773
Beata Detkova <beata.detkova.jr@gmail.com>
2020-03-13 17:38:55 +0100

    Updates

---
+++ precache-manifest.288af74ef80e5eb1186ec7c3d066c06d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7ddd1f664b404abaf19173f04718fe",
+    "url": "/catfriends/index.html"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/catfriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "d85d07b89b0f0c6b2b38",
+    "url": "/catfriends/static/css/main.3e4246df.chunk.css"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/catfriends/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/catfriends/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d85d07b89b0f0c6b2b38",
+    "url": "/catfriends/static/js/main.e8d4f1bd.chunk.js"
+  },
+  {
+    "revision": "1f51c23de84d9b861617",
+    "url": "/catfriends/static/js/runtime-main.9df82da5.js"
+  },
+  {
+    "revision": "2e5ec5b3e538df515284bcc246b0629d",
+    "url": "/catfriends/static/media/Strawberry Shortcake Supplement Logo.2e5ec5b3.woff"
+  }
+]);
